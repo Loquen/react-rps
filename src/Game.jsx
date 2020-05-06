f282905b09d15a801ed46dc77c03bcd415b5e843
Loquen Jones <lrjones416@gmail.com>
2020-05-06 10:37:56 -0700

    intial commit

---
+++ src/Game.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Game(props) {
+  return (
+    <div>
+      You Picked
+      The House Picked
+    
+      You Win
+      You Lose
+    
+      Play Again
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/Rules.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rules(props) {
+  return (
+    <div>
+      Rules
+    </div>
+  );
+}
+
+export default Rules;
